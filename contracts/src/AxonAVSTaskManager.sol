6bbd3e5ded82b018e3a575d9dfb2791c229b4e12
Defcon023 <manmeetparmar23@gmail.com>
2024-05-07 20:30:51 +0530

    update remappings

--- contracts/src/AxonAVSTaskManager.sol
+++ contracts/src/AxonAVSTaskManager.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
 
 import "@openzeppelin-upgrades/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin-upgrades/contracts/access/OwnableUpgradeable.sol";
 import "@eigenlayer/contracts/permissions/Pausable.sol";
-import "eigenlayer-middleware/src/interfaces/IServiceManager.sol";
-import {BLSApkRegistry} from "eigenlayer-middleware/src/BLSApkRegistry.sol";
-import {RegistryCoordinator} from "eigenlayer-middleware/src/RegistryCoordinator.sol";
-import {BLSSignatureChecker, IRegistryCoordinator} from "eigenlayer-middleware/src/BLSSignatureChecker.sol";
-import {OperatorStateRetriever} from "eigenlayer-middleware/src/OperatorStateRetriever.sol";
-import "eigenlayer-middleware/src/libraries/BN254.sol";
+import "@eigenlayer-middleware/src/interfaces/IServiceManager.sol";
+import {BLSApkRegistry} from "@eigenlayer-middleware/src/BLSApkRegistry.sol";
+import {RegistryCoordinator} from "@eigenlayer-middleware/src/RegistryCoordinator.sol";
+import {BLSSignatureChecker, IRegistryCoordinator} from "@eigenlayer-middleware/src/BLSSignatureChecker.sol";
+import {OperatorStateRetriever} from "@eigenlayer-middleware/src/OperatorStateRetriever.sol";
+import "@eigenlayer-middleware/src/libraries/BN254.sol";
 import "./IAxonAVSTaskManager.sol";
 
 contract AxonAVSTaskManager is
