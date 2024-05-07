4c493bd9343e5c8f27df56764f2d4d40fdff350d
Defcon023 <manmeetparmar23@gmail.com>
2024-05-07 20:35:04 +0530

    feat: AxonAVSServiceManager contract

---
+++ contracts/src/AxonAVSServiceManager.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "@eigenlayer/contracts/libraries/BytesLib.sol";
+import "./IAxonAVSTaskManager.sol";
+import "@eigenlayer-middleware/src/ServiceManagerBase.sol";
+
+contract AxonAVSServiceManager is ServiceManagerBase {
+    using BytesLib for bytes;
+
+    IAxonAVSTaskManager
+        public immutable axonAVSTaskManager;
+
+    /// @notice when applied to a function, ensures that the function is only callable by the `registryCoordinator`.
+    modifier onlyAxonAVSTaskManager() {
+        require(
+            msg.sender == address(axonAVSTaskManager),
+            "onlyAxonAVSTaskManager: not from credible squaring task manager"
+        );
+        _;
+    }
+
+    constructor(
+        IAVSDirectory _avsDirectory,
+        IRegistryCoordinator _registryCoordinator,
+        IStakeRegistry _stakeRegistry,
+        IAxonAVSTaskManager _axonAVSTaskManager
+    )
+        ServiceManagerBase(
+            _avsDirectory,
+            IPaymentCoordinator(address(0)),
+            _registryCoordinator,
+            _stakeRegistry
+        )
+    {
+        axonAVSTaskManager = _axonAVSTaskManager;
+    }
+}
